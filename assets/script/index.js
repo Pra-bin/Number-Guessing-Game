2070ef396e2315c842c4b5a113fc3ca227fc909e
pra-bin <prabinbk06@gmail.com>
2024-06-23 23:00:25 +0545

    source code

---
+++ assets/script/index.js
@@ -0,0 +1,37 @@
+const userNumber = document.getElementById("userNumber");
+const Btn = document.getElementById("button");
+const userGuess = document.getElementById('userGuessValue');
+const previousMatchedGuess = document.getElementById('previousGuessValue');
+const img = document.getElementById("image");
+const triesValue = document.getElementById("triesValue");
+let leftNumbers = +triesValue.innerText;
+const previousMatchedGuessValue = [];
+const previousGuess = [];
+let regx = /[0-9]/;
+Btn.addEventListener("click", function () {
+    if (leftNumbers > 0) {
+        if (regx.test(+userNumber.value)&&+userNumber.value<10&&+userNumber.value>=0) {
+            num = Math.floor(Math.random() * 10);
+            previousGuess + previousGuess.push(`${+userNumber.value}`);
+            userGuess.innerText = previousGuess;
+            if (+userNumber.value === num) {
+                img.classList.add("show");
+                leftNumbers = leftNumbers + 1;
+                previousMatchedGuessValue + previousMatchedGuessValue.push(`${userNumber.value}`);
+                previousMatchedGuess.innerText = previousMatchedGuessValue;
+
+            } else {
+                img.classList.remove("show");
+                leftNumbers = leftNumbers - 1;
+            }
+            triesValue.innerText = `${+leftNumbers}`;
+        }
+        else{
+            window.alert("wrong input");
+
+        }
+
+    } else {
+        window.alert("your try number completed:");
+    }
+});
